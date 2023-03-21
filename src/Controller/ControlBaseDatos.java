f034670b173cae8294c5ff30eb6aaa58fa85b610
juanp <juanpabloordonezjimenez@gmail.com>
2023-03-20 21:15:28 -0500

    subida4

--- src/Controller/ControlBaseDatos.java
+++ src/Controller/ControlBaseDatos.java
@@ -3,8 +3,7 @@ package Controller;
 public class ControlBaseDatos {
 
 	public void insert(String datos) {
-		// TODO - implement ControlBaseDatos.insert
-		throw new UnsupportedOperationException();
+
 	}
 
 }
--- src/Controller/CtrlServicio.java
+++ src/Controller/CtrlServicio.java
@@ -3,23 +3,19 @@ package Controller;
 public class CtrlServicio {
 
 	public void añadirServicio() {
-		// TODO - implement CtrlServicio.añadirServicio
-		throw new UnsupportedOperationException();
+
 	}
 
 	public void actualizarServicio() {
-		// TODO - implement CtrlServicio.actualizarServicio
-		throw new UnsupportedOperationException();
+
 	}
 
 	public void inactivarServicio() {
-		// TODO - implement CtrlServicio.inactivarServicio
-		throw new UnsupportedOperationException();
+
 	}
 
 	public void consultarServicio() {
-		// TODO - implement CtrlServicio.consultarServicio
-		throw new UnsupportedOperationException();
+
 	}
 
 }
