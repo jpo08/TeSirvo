d24eca7c0742590d86e908ec9feb81d5cfc681d3
juanp <juanpabloordonezjimenez@gmail.com>
2023-03-21 11:17:57 -0500

    subida7

--- src/model/Internet.java
+++ src/model/Internet.java
@@ -3,4 +3,9 @@ package model;
 public class Internet extends Servicios{
 
     private ContractType contractType;
+
+
+    public Internet(String codigoContrato) {
+        super(codigoContrato);
+    }
 }
