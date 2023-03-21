15fdaac3dd529f90c5fec8649ea6086a2459867e
juanp <juanpabloordonezjimenez@gmail.com>
2023-03-20 21:50:27 -0500

    subida4

--- src/model/EmpresaServicios.java
+++ src/model/EmpresaServicios.java
@@ -5,7 +5,25 @@ import java.util.List;
 
 public class EmpresaServicios {
 
-	private ArrayList<Cliente> clientes= new ArrayList<>() {};
+	private List<Cliente> clientes=new ArrayList<Cliente>();
+
+	public void addClient(String n, String id) {
+
+		Cliente searched = null;
+		for (int i = 0; i < clientes.size() && searched==null; i++) {
+			Cliente current = clientes.get(i);
+			if(current.getName().equals(n)) {
+				searched = current;
+			}
+		}
+
+		if(searched==null) {
+			Cliente newclient = new Cliente(n, id);
+			clientes.add(newclient);
+		}
+	}
+
+
 
 
 }
