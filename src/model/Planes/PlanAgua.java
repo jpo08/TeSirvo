fd733c62faa9acc95dea90a472dac7abcaca17b6
Jeison Lasprilla <1114238132@u.icesi.edu.co>
2023-05-04 12:02:09 -0500

    UMLV2

---
+++ src/model/Planes/PlanAgua.java
@@ -0,0 +1,10 @@
+package model.Planes;
+
+import java.util.Date;
+
+public class PlanAgua extends Plan{
+
+    public PlanAgua(double valorOfrecido, String id, String name, Date videnciaLower, Date getVidenciaUpper, double valorDeCapacidad) {
+        super(valorOfrecido, id, name, videnciaLower, getVidenciaUpper, valorDeCapacidad);
+    }
+}
